85eba26cb0d493cf45f33a1ed1d43f80bed56588
RubenDjOn <ruben@indahouser.com>
2015-09-24 22:57:23 +0000

    - Added basic integration with DashGum Free Bootstrap Theme

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -18,12 +18,18 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'css/style.css',
+        'css/style-responsive.css',
     ];
     public $js = [
+        'js/jquery.dcjqaccordion.2.7.js',
+        'js/jquery.scrollTo.min.js',
+        'js/jquery.nicescroll.js',
+        'js/common-scripts.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
     ];
 }
